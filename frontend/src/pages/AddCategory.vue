df221d6936f6b9ba550c86a140d9fb8b12b8c7f5
euruk <efeuruk1@gmail.com>
2021-02-03 17:37:39 +0300

    Add Category page

--- frontend/src/pages/AddCategory.vue
+++ frontend/src/pages/AddCategory.vue
@@ -1,10 +1,59 @@
 <template>
-  <div>Add Category</div>
+  <div>
+    <h1>Kategori Ekle</h1>
+    <form @submit.prevent="submit()">
+      <Input
+        label="İsim"
+        v-model="kategori"
+        :isError="$v.kategori.$error"
+        errorText="Kategori alanı zorunludur"
+      />
+      <button type="submit" class="btn btn-dark btn-block">Kaydet</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import Input from "@/components/Input";
+import axios from "axios";
+import { required, alphaNum } from "vuelidate/lib/validators";
+
 export default {
   name: "AddCategory",
+  components: {
+    Input,
+  },
+  data() {
+    return {
+      kategori: "",
+    };
+  },
+  validations: {
+    kategori: {
+      required,
+      alphaNum,
+    },
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$error) return;
+      this.addCategory();
+    },
+    addCategory() {
+      axios
+        .post("/api/createCategory", {
+          name: this.kategori,
+        })
+        .then((response) => {
+          console.log(response);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>
 
